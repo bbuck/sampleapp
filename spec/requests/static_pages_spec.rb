6b2ead0dc7fad4884f28ab21610e91b52f366bd2
Brandon Buck <lordizuriel@gmail.com>
2012-07-14 19:42:03 -0500

    Implement exercise problems.

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
+  describe "Home Page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', text: 'Sample App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', text: "#{base_title} | Home")
+    end
+  end
+
+  describe "Help Page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', text: 'Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', text: "#{base_title} | Help")
+    end
+  end
+
+  describe "About Page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', text: 'About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', text: "#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact Page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: "Contact Us")
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', text: "#{base_title} | Contact Us")
+    end
+  end
+end
