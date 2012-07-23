21d0a89c99b79c0f2f4186a883701cf04058f3c8
Brandon Buck <lordizuriel@gmail.com>
2012-07-22 20:38:22 -0500

    Finish social changes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
-  has_many :reverse_relationships, foreign_key: "followed_id", 
+  has_many :reverse_relationships, foreign_key: "followed_id",
     class_name: "Relationship", dependent: :destroy
   has_many :followers, through: :reverse_relationships
 
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
   before_save :create_remember_token
 
   def feed
-    Micropost.where "user_id = ?", id
+    Micropost.from_users_followed_by self
   end
 
   def following?(other_user)
